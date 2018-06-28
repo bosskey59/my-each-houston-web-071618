89e15d6e58a74f11bc0ebc0aac4ef9c315d0b2be
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-28 16:15:41 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,10 @@ def my_each(array) # put argument(s) here
   # code here
   i=0
   while i<array.length
-  
+    i=+1
+  end
 end
-  
-end
+
 collection = [1, 2, 3, 4]
 my_each(collection) do |i|
   puts i
